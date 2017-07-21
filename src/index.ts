3683ffc2ea7269485779f5303c061ce636938387
unHealthy <zhutian.chen@outlook.com>
2017-07-21 14:50:00 +0800

    feat: init, convert to ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,76 @@
-import { normalize, basename, extname, resolve, parse, sep } from 'path';
+import { createReadStream } from 'fs';
+import { Context } from 'koa';
+import { basename, extname, normalize, parse, resolve, sep } from 'path';
+import { exists, HttpError, resolvePath, stat } from './utils';
 
-const resolvePath = require('resolve-path')
-const fs = require('mz/fs')
+export interface StaticServerOptions {
+    defer?: boolean;
+    index?: string | false;
+    maxAge?: number;
+    immutable?: boolean;
+    hidden?: boolean;
+    format?: boolean;
+    extensions?: string[] | false;
+    brotli?: boolean;
+    gzip?: boolean;
+    setHeaders?: (...args: any[]) => any;
+}
+
+const staticServerDefaultOptions = {
+    index: 'index.html' as 'index.html',
+    defer: false as false,
+    maxAge: 0 as 0,
+    immutable: false as false,
+    hidden: false as false,
+    format: false as false,
+    extensions: false as false,
+    brotli: false as false,
+    gzip: false as false,
+};
+
+export function serve(root: string = '', opts: StaticServerOptions = {}) {
+
+    const options = Object.assign({}, staticServerDefaultOptions, opts);
+    if (options.defer) {
+        return async (ctx: Context, next: () => Promise<any>) => {
+            await next();
+
+            if (ctx.method !== 'HEAD' && ctx.method !== 'GET') {
+                return;
+            }
+            // response is already handled
+            if (ctx.body !== null || ctx.status !== 404) {
+                return;
+            }
+            try {
+                await send(ctx, root, options);
+            } catch (err) {
+                if (err.status !== 404) {
+                    throw err;
+                }
+            }
+        };
+    }
+
+    return async (ctx: Context, next: () => Promise<any>) => {
+        let done: string | false = false;
+
+        if (ctx.method === 'HEAD' || ctx.method === 'GET') {
+            try {
+                done = await send(ctx, root, options);
+            } catch (err) {
+                if (err.status !== 404) {
+                    throw err;
+                }
+            }
+        }
+
+        if (!done) {
+            await next();
+        }
+    };
+
+}
 
 /**
  * Send file at `path` with the
@@ -13,137 +82,151 @@ const fs = require('mz/fs')
  * @return {Function}
  * @api public
  */
+async function send(ctx: Context, rootPath: string, opts: StaticServerOptions & typeof staticServerDefaultOptions) {
 
-export async function send(ctx, path, opts = {}) {
+    const root = normalize(resolve(rootPath));
+    const {
+        index,
+        maxAge,
+        immutable,
+        hidden,
+        format,
+        extensions,
+        brotli,
+        gzip,
+        setHeaders } = opts;
 
-    // options
-    const root = opts.root ? normalize(resolve(opts.root)) : ''
-    const trailingSlash = path[path.length - 1] === '/'
-    path = path.substr(parse(path).root.length)
-    const index = opts.index
-    const maxage = opts.maxage || opts.maxAge || 0
-    const immutable = opts.immutable || false
-    const hidden = opts.hidden || false
-    const format = opts.format !== false
-    const extensions = Array.isArray(opts.extensions) ? opts.extensions : false
-    const brotli = opts.brotli !== false
-    const gzip = opts.gzip !== false
-    const setHeaders = opts.setHeaders
-
-    if (setHeaders && typeof setHeaders !== 'function') {
-        throw new TypeError('option setHeaders must be function')
-    }
-
+    let path: string = ctx.path.substr(parse(ctx.path).root.length);
     // normalize path
-    path = decode(path)
-
-    if (path === -1) return ctx.throw(400, 'failed to decode')
+    const decodedPath = decode(path);
+    if (decodedPath === -1) {
+        return ctx.throw(400, 'failed to decode');
+    }
+    path = decodedPath;
 
     // index file support
-    if (index && trailingSlash) path += index
+    const trailingSlash = ctx.path[ctx.path.length - 1] === '/';
+    if (index && trailingSlash) {
+        path += index;
+    }
 
-    path = resolvePath(root, path)
-
+    path = resolvePath(root, path);
     // hidden file support, ignore
-    if (!hidden && isHidden(root, path)) return
+    if (!hidden && isHidden(root, path)) {
+        return false;
+    }
 
     // serve brotli file when possible otherwise gzipped file when possible
-    if (ctx.acceptsEncodings('br', 'deflate', 'identity') === 'br' && brotli && (await fs.exists(path + '.br'))) {
-        path = path + '.br'
-        ctx.set('Content-Encoding', 'br')
-        ctx.res.removeHeader('Content-Length')
-    } else if (ctx.acceptsEncodings('gzip', 'deflate', 'identity') === 'gzip' && gzip && (await fs.exists(path + '.gz'))) {
-        path = path + '.gz'
-        ctx.set('Content-Encoding', 'gzip')
-        ctx.res.removeHeader('Content-Length')
+    if (ctx.acceptsEncodings('br', 'deflate', 'identity') === 'br' &&
+        brotli &&
+        (await exists(path + '.br'))) {
+        path = path + '.br';
+        ctx.set('Content-Encoding', 'br');
+        ctx.res.removeHeader('Content-Length');
+    } else if (ctx.acceptsEncodings('gzip', 'deflate', 'identity') === 'gzip' &&
+        gzip &&
+        (await exists(path + '.gz'))) {
+        path = path + '.gz';
+        ctx.set('Content-Encoding', 'gzip');
+        ctx.res.removeHeader('Content-Length');
     }
 
     if (extensions && !/\..*$/.exec(path)) {
-        const list = [].concat(extensions)
-        for (let i = 0; i < list.length; i++) {
-            let ext = list[i]
-            if (typeof ext !== 'string') {
-                throw new TypeError('option extensions must be array of strings or false')
+        for (let ext of extensions) {
+            if (!/^\./.exec(ext)) {
+                ext = '.' + ext;
             }
-            if (!/^\./.exec(ext)) ext = '.' + ext
-            if (await fs.exists(path + ext)) {
-                path = path + ext
-                break
+            if (await exists(path + ext)) {
+                path = path + ext;
+                break;
             }
         }
     }
 
     // stat
-    let stats
+    let stats;
     try {
-        stats = await fs.stat(path)
+        stats = await stat(path);
 
         // Format the path to serve static file servers
         // and not require a trailing slash for directories,
         // so that you can do both `/directory` and `/directory/`
         if (stats.isDirectory()) {
             if (format && index) {
-                path += '/' + index
-                stats = await fs.stat(path)
+                path += '/' + index;
+                stats = await stat(path);
             } else {
-                return
+                return false;
             }
         }
     } catch (err) {
-        const notfound = ['ENOENT', 'ENAMETOOLONG', 'ENOTDIR']
-        if (notfound.includes(err.code)) {
-            throw createError(404, err)
+        const notfound = new Set(['ENOENT', 'ENAMETOOLONG', 'ENOTDIR']);
+        if (notfound.has(err.code)) {
+            throw new HttpError(404, err);
         }
-        err.status = 500
-        throw err
+        err.status = 500;
+        throw err;
     }
 
-    if (setHeaders) setHeaders(ctx.res, path, stats)
+    if (setHeaders) {
+        setHeaders(ctx.res, path, stats);
+    }
 
     // stream
-    ctx.set('Content-Length', stats.size)
-    if (!ctx.response.get('Last-Modified')) ctx.set('Last-Modified', stats.mtime.toUTCString())
-    if (!ctx.response.get('Cache-Control')) {
-        const directives = ['max-age=' + (maxage / 1000 | 0)]
-        if (immutable) {
-            directives.push('immutable')
-        }
-        ctx.set('Cache-Control', directives.join(','))
+    ctx.set('Content-Length', stats.size.toString());
+    if (!ctx.response.get('Last-Modified')) {
+        ctx.set('Last-Modified', stats.mtime.toUTCString());
     }
-    ctx.type = type(path)
-    ctx.body = fs.createReadStream(path)
+    if (!ctx.response.get('Cache-Control')) {
+        const directives = ['max-age=' + Math.ceil(maxAge / 1000)];
+        if (immutable) {
+            directives.push('immutable');
+        }
+        ctx.set('Cache-Control', directives.join(','));
+    }
+    ctx.type = type(path);
+    ctx.body = createReadStream(path);
 
-    return path
+    return path;
 }
 
 /**
  * Check if it's hidden.
+ *
+ * @param {string} root
+ * @param {string} path
+ * @returns
  */
-
-function isHidden(root, path) {
-    path = path.substr(root.length).split(sep)
-    for (let i = 0; i < path.length; i++) {
-        if (path[i][0] === '.') return true
+function isHidden(root: string, path: string) {
+    const filePath = path.substr(root.length).split(sep);
+    for (let i = 0, len = filePath.length; i < len; ++i) {
+        if (filePath[i][0] === '.') {
+            return true;
+        }
     }
-    return false
+    return false;
 }
 
 /**
  * File type.
+ *
+ * @param {string} file
+ * @returns
  */
-
-function type(file) {
-    return extname(basename(file, '.gz'))
+function type(file: string) {
+    return extname(basename(file, '.gz'));
 }
 
 /**
  * Decode `path`.
+ *
+ * @param {string} path
+ * @returns
  */
-
-function decode(path) {
+function decode(path: string) {
     try {
-        return decodeURIComponent(path)
+        return decodeURIComponent(path);
     } catch (err) {
-        return -1
+        return -1;
     }
 }
